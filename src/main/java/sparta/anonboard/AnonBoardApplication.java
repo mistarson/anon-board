e12d3fdd0fe425558f212b9933256f6aa43c7e2b
mistarson <yeowuli2@naver.com>
2023-11-03 10:56:23 +0900

    init Commit

---
+++ src/main/java/sparta/anonboard/AnonBoardApplication.java
@@ -0,0 +1,13 @@
+package sparta.anonboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sparta/anonboard/AnonBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.anonboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
