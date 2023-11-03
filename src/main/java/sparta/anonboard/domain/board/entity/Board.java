064c1ebe844c8088975629734e079e77b7209d93
mistarson <yeowuli2@naver.com>
2023-11-03 19:49:55 +0900

    Feat: 게시판 엔티티 설계 및 구현

---
+++ src/main/java/sparta/anonboard/domain/board/entity/Board.java
@@ -0,0 +1,39 @@
+package sparta.anonboard.domain.board.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sparta.anonboard.domain.common.BaseTimeEntity;
+
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String writer;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Builder
+    public Board(String writer, String content, String password) {
+        this.writer = writer;
+        this.content = content;
+        this.password = password;
+    }
+}
---
+++ src/main/java/sparta/anonboard/domain/common/BaseTimeEntity.java
@@ -0,0 +1,23 @@
+package sparta.anonboard.domain.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createTime;
+
+    @LastModifiedDate
+    private LocalDateTime updateTime;
+}
